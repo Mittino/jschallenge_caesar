49c1d65efce759df06c3200e96f13f569ff7255d
mittino <annamefford@gmail.com>
2017-01-17 13:26:48 -0700

    identifying letter as upper or lowercase

--- caesarShift.js
+++ caesarShift.js
@@ -1,4 +1,31 @@
+(function(){
+  "use strict";
+  var _ = require('lodash');
+
+  //input a letter
+  //determine if upper or lowercase - regex?
+  //Find position in alphabet
+  //Move over 3
+
+  var lowercaseAlphabet = 'abcdefghijklmnopqrstuvwxyz';
+  var uppercaseAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  function ceasarShift(letter){
+    var regexLower = new RegExp(/[/a-z]/g);
+    var regexUpper = new RegExp(/[/A-Z]/g);
+    if(regexLower.test(letter)){
+      console.log('lowercase');
+    } else if (regexUpper.test(letter)){
+      console.log("uppercase");
+    }
+
+  }
+
+ceasarShift('a');
+ceasarShift('A');
 
 
 
-module.exports = {}
+  module.exports = {}
+
+})();
